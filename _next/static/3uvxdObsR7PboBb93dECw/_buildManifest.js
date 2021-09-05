18d89f16969a459049912380bbf89d7ba28c2cec
NonFungiblePerson <NonFungiblePerson@users.noreply.github.com>
2021-09-05 21:05:00 +0000

    deploy: 3cfcf5fc50d90c9885ef56c1520b69544c91a1f9

---
+++ _next/static/3uvxdObsR7PboBb93dECw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/736-fb17a22443469fe05c86.js","static/chunks/pages/index-7478334ecb8df502ea4c.js"],"/_error":["static/chunks/pages/_error-f5e67a79c709c2df06b8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/poND9ngkKPP3hFlToZ7f9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/736-fb17a22443469fe05c86.js","static/chunks/pages/index-c397a9d48366baee6285.js"],"/_error":["static/chunks/pages/_error-f5e67a79c709c2df06b8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
